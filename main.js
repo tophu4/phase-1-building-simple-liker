7f6ad097c248683989ec3af81ca69e3d070a9a40
Christopher Dean Canlas <chrisdcanlas@gmail.com>
2023-03-25 21:04:32 -0700

    complete lab

--- main.js
+++ main.js
@@ -3,9 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeButtons = document.querySelectorAll(".like-glyph")
 
+// if a button in likeButtons is clicked, switch after successful server response
+likeButtons.forEach(btn => {
+  btn.addEventListener("click",() => {
+    mimicServerCall()
+    .then(res => {
+      if(btn.innerHTML == EMPTY_HEART){
+        btn.innerHTML = FULL_HEART;
+        btn.classList.add("activated-heart")
+      }
+      else{
+        btn.innerHTML = EMPTY_HEART;
+        btn.classList.remove("activated-heart")
+      }
+    })
+    .catch(error => {
+      const errorModal = document.querySelector("#modal");
+      const errorModalMessage = document.querySelector("#modal-message");
 
+      errorModal.classList.remove("hidden");
+      errorModalMessage.innerHTML = "server error";
 
+      setTimeout(function(){
+        errorModal.classList.add("hidden");
+      },3000);
+    })
+  })
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
